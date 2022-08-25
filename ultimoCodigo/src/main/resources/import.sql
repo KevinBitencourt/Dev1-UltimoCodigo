4e65e08b48105453189fda868a2a734a087f3fb1
KevinBitencourt <kevin.bitencourt.silva@gmail.com>
2022-08-25 14:27:35 -0300

    UltimoCodigo Final

--- ultimoCodigo/src/main/resources/import.sql
+++ ultimoCodigo/src/main/resources/import.sql
@@ -1,11 +1,11 @@
-insert into produto (id, nome) values (1, 'Tailandesa');
-insert into produto (id, nome) values (2, 'Indiana');
+insert into produto (id, nome) values (1, 'Bola');
+insert into produto (id, nome) values (2, 'Chinelo');
 
-insert into cliente (id, nome, produto_id) values (1, 'Thai Gourmet', 1);
-insert into cliente (id, nome, produto_id) values (2, 'Thai Delivery', 1);
-insert into cliente (id, nome, produto_id) values (3, 'Tuk Tuk Comida Indiana', 2);
+insert into cliente (id, nome, produto_id) values (1, 'João', 1);
+insert into cliente (id, nome, produto_id) values (2, 'Maria', 1);
+insert into cliente (id, nome, produto_id) values (3, 'Pedrinho', 2);
 
-insert into venda (id, descricao) values (1, 'Cartão de crédito');
-insert into venda (id, descricao) values (2, 'Cartão de débito');
-insert into venda (id, descricao) values (3, 'Dinheiro');
+insert into venda (id, nome) values (1, 'Compra 1');
+insert into venda (id, nome) values (2, 'Compra 2');
+insert into venda (id, nome) values (3, 'Compra 3');
 insert into Cliente_Venda (cliente_id, venda_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
